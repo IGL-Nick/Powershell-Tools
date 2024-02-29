33681ab0f64119dee130e671caedeb351040165b
IGL-Nick <159208587+IGL-Nick@users.noreply.github.com>
2024-02-29 14:47:17 -0500

    Update Start-Install.psm1

--- Scripts/Start-Install.psm1
+++ Scripts/Start-Install.psm1
@@ -1,1 +1,35 @@
+Function Start-Install{
+Param (
+        [Parameter(Mandatory=$true)]
+        [String]$URL
+)
+
+$workingDirectory = 'c:\drop'
+
+
+$asset = (Get-RedirectedUrl $URL).split('/')[-1]
+
+$installFolder = $asset.split('.')[0]
+
+$installer = "$workingDirectory\$installFolder\$asset"
+
+
+Try{mkdir "$workingDirectory\$installFolder"
+    Write-Host 'Creating working directory'
+}
+catch{
+    Write-Host 'Working directory exists'
+}
+
+if(!(Test-Path $installer)){
+# download installer
+
+Invoke-WebRequest -Uri $URL -OutFile $installer
+}
+
+return $installer
+
+
+}
+
 
