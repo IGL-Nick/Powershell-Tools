e1a98d9207a6f00c2ae0a96d482f52ad404f7e0a
IGL-Nick <159208587+IGL-Nick@users.noreply.github.com>
2024-04-26 15:04:17 -0400

    Update and rename Install-Windows_Server_2022 to Get-WindowsServer2022.ps1

--- Installers/Get-WindowsServer2022.ps1
+++ Installers/Get-WindowsServer2022.ps1
@@ -1,5 +1,5 @@
 #Command to kick off the setup script. Run in a 
-#(mkdir c:\drop & cd c:\drop && curl -o cascade.ps1 https://raw.githubusercontent.com/IGL-Nick/RepoReplicator/main/cascade.ps1 && powershell.exe -executionpolicy bypass -noprofile -file .\cascade.ps1)
+#(mkdir c:\drop & cd c:\drop && curl -o Get-WindowsServer2022.ps1 https://raw.githubusercontent.com/IGL-Nick/Powershell-Tools/main/Installers/Get-WindowsServer2022.ps1 && powershell.exe -executionpolicy bypass -noprofile -file .\Get-WindowsServer2022.ps1)
 
 #check for elevated permissions
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
